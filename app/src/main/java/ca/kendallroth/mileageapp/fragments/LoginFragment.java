05be5ec0de2e3b7c3f7c9a0f06b00b08525ac2a8
Kendall Roth <kendall@focus21.io>
2017-09-27 19:55:21 -0400

    #16 Copy logic from archived Activities into Login/Register fragments

--- app/src/main/java/ca/kendallroth/mileageapp/fragments/LoginFragment.java
+++ app/src/main/java/ca/kendallroth/mileageapp/fragments/LoginFragment.java
@@ -1,18 +1,51 @@
 package ca.kendallroth.mileageapp.fragments;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+
+import org.dom4j.Document;
+import org.dom4j.Node;
+
+import java.util.List;
 
 import ca.kendallroth.mileageapp.R;
+import ca.kendallroth.mileageapp.activities.HomeActivity;
+import ca.kendallroth.mileageapp.utils.XMLFileUtils;
 
 /**
  * Fragment to allow users to login (or automatically authenticate)
  */
 public class LoginFragment extends Fragment {
+
+  // Login asynchronous task
+  private LoginTask mAuthTask = null;
+
+  // UI references.
+  private Button mLoginButton;
+  private EditText mEmailInput;
+  private EditText mPasswordInput;
+  private TextInputLayout mEmailViewLayout;
+  private TextInputLayout mPasswordViewLayout;
+  private ProgressDialog mProgressDialog;
+  private View mFormLayout;
+
   // Helper constants for argument variable names
   private static final String ARG_TITLE = "mTitle";
 
@@ -49,7 +82,131 @@ public class LoginFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_login, container, false);
+    View loginView = inflater.inflate(R.layout.fragment_login, container, false);
+
+    // Email input
+    mEmailInput = (EditText) loginView.findViewById(R.id.email_input);
+    mEmailViewLayout = (TextInputLayout) loginView.findViewById(R.id.email_layout);
+
+    // Password input
+    mPasswordInput = (EditText) loginView.findViewById(R.id.password_input);
+    mPasswordViewLayout = (TextInputLayout) loginView.findViewById(R.id.password_layout);
+    mPasswordInput.setOnEditorActionListener(new OnEditorActionListener() {
+      @Override
+      public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+        // Attempt login on <Enter> keypress while the Password field has focus
+        if (id == R.id.login || id == EditorInfo.IME_NULL) {
+          attemptLogin();
+          return true;
+        }
+        return false;
+      }
+    });
+
+    // Login button
+    mLoginButton = (Button) loginView.findViewById(R.id.login_button);
+    mLoginButton.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        attemptLogin();
+      }
+    });
+
+    // Progress dialog
+    mProgressDialog = new ProgressDialog(loginView.getContext());
+    mProgressDialog.setMessage(getString(R.string.progress_message_login_attempt));
+
+    // Layout views
+    mFormLayout = loginView.findViewById(R.id.form_layout);
+
+    return loginView;
+  }
+
+  /**
+   * Attempts to sign in or register the account specified by the login form.
+   * If there are form errors (invalid email, missing fields, etc.), the
+   * errors are presented and no actual login attempt is made.
+   */
+  private void attemptLogin() {
+    if (mAuthTask != null) {
+      return;
+    }
+
+    // Reset errors.
+    mEmailViewLayout.setError(null);
+    mPasswordViewLayout.setError(null);
+
+    // Store values at the time of the login attempt.
+    String email = mEmailInput.getText().toString();
+    String password = mPasswordInput.getText().toString();
+
+    boolean cancel = false;
+    View focusView = null;
+
+    // Check for a valid password, if the user entered one.
+    if (TextUtils.isEmpty(password)) {
+      mPasswordViewLayout.setError(getString(R.string.error_field_required));
+      focusView = mPasswordInput;
+      cancel = true;
+    }
+    else if (!isPasswordValid(password)) {
+      mPasswordViewLayout.setError(getString(R.string.error_invalid_password));
+      focusView = mPasswordInput;
+      cancel = true;
+    }
+
+    // Check for a valid email address (is last to set focus properly).
+    if (TextUtils.isEmpty(email)) {
+      mEmailViewLayout.setError(getString(R.string.error_field_required));
+      focusView = mEmailInput;
+      cancel = true;
+    } else if (!isEmailValid(email)) {
+      mEmailViewLayout.setError(getString(R.string.error_invalid_email));
+      focusView = mEmailInput;
+      cancel = true;
+    }
+
+    if (cancel) {
+      // Ignore login attempt (due to error) and set focus to last field with error
+      focusView.requestFocus();
+    } else {
+      // Show a progress spinner, and kick off a background task to perform the user login attempt.
+      mProgressDialog.show();
+      mAuthTask = new LoginTask(email, password);
+      mAuthTask.execute((Void) null);
+    }
+  }
+
+  /**
+   * Validate the email
+   * @param email Email address
+   * @return Whether the email address is valid
+   */
+  private boolean isEmailValid(String email) {
+    //TODO: Replace with updated logic
+    return email.contains("@") && email.contains(".");
+  }
+
+  /**
+   * Validate the password
+   * @param password Password
+   * @return Whether the password is valid
+   */
+  private boolean isPasswordValid(String password) {
+    //TODO: Replace with updated logic
+    return password.length() > 4;
+  }
+
+
+  /**
+   * Clear the inputs and errors
+   */
+  private void clearLoginInputs() {
+    mEmailInput.setText("");
+    mEmailViewLayout.setError(null);
+
+    mPasswordInput.setText("");
+    mPasswordViewLayout.setError(null);
   }
 
   // TODO: Rename method, update argument and hook method into UI event
@@ -90,4 +247,91 @@ public class LoginFragment extends Fragment {
     // TODO: Update argument type and name
     void onFragmentInteraction(Uri uri);
   }*/
+
+
+  /**
+   * Represents an asynchronous login/registration task used to authenticate the user.
+   */
+  public class LoginTask extends AsyncTask<Void, Void, Boolean> {
+
+    private final String mEmail;
+    private final String mPassword;
+
+    LoginTask(String email, String password) {
+      mEmail = email;
+      mPassword = password;
+    }
+
+    @Override
+    protected Boolean doInBackground(Void... params) {
+      // TODO: attempt authentication against a network service.
+
+      try {
+        // Simulate network access.
+        Thread.sleep(2000);
+      } catch (InterruptedException e) {
+        return false;
+      }
+
+      Document document;
+
+      try {
+        // Read XML file with user information
+        document = XMLFileUtils.getFile(getActivity().getBaseContext(), XMLFileUtils.USERS_FILE_NAME);
+
+        // Select all the "user" nodes in the document
+        List<Node> users = document.selectNodes("/users/user");
+
+        Log.d("MileageApp", String.format("Login attempt from '%s' with password '%s'", mEmail, mPassword));
+
+        for (Node user : users) {
+          // Compare the entered email and password against the "registered" accounts
+          if (user.valueOf("@email").equals(mEmail)) {
+            boolean validAuthAttempt = user.valueOf("@password").equals(mPassword);
+
+            Log.d("MileageApp.auth", String.format("Login attempt %s", validAuthAttempt ? "successful" : "failed"));
+
+            return validAuthAttempt;
+          }
+        }
+      } catch (Exception e) {
+        // Return false (no match) if the file parsing fails or throws an exception
+        return false;
+      }
+
+      return false;
+    }
+
+    @Override
+    protected void onPostExecute(final Boolean success) {
+      mAuthTask = null;
+      mProgressDialog.dismiss();
+
+      if (success) {
+        // Set navigation history (home) and start the Home activity
+        Intent homeActivityIntent = new Intent(getContext().getApplicationContext(), HomeActivity.class);
+        homeActivityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_TASK_ON_HOME);
+        startActivity(homeActivityIntent);
+        getActivity().finish();
+      } else {
+        mPasswordViewLayout.setError(getString(R.string.error_incorrect_password));
+        mPasswordInput.requestFocus();
+      }
+
+      // TODO: Figure out why Snackbar never shows (even for failure)
+      // Define a snackbar based on the operation status
+      CharSequence snackbarResource = success
+          ? getString(R.string.success_login)
+          : getString(R.string.failure_login);
+      Snackbar resultSnackbar = Snackbar.make(mFormLayout, snackbarResource, Snackbar.LENGTH_SHORT);
+      resultSnackbar.show();
+    }
+
+    @Override
+    protected void onCancelled() {
+      // NOTE: Should logging in be canceallable?
+      mAuthTask = null;
+      mProgressDialog.dismiss();
+    }
+  }
 }
--- app/src/main/java/ca/kendallroth/mileageapp/fragments/RegisterFragment.java
+++ app/src/main/java/ca/kendallroth/mileageapp/fragments/RegisterFragment.java
@@ -1,18 +1,50 @@
 package ca.kendallroth.mileageapp.fragments;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.Node;
+
+import java.util.List;
 
 import ca.kendallroth.mileageapp.R;
+import ca.kendallroth.mileageapp.utils.XMLFileUtils;
 
 /**
  * Fragment for enabling a user to register for the app
  */
 public class RegisterFragment extends Fragment {
+
+  // Create Account asynchronous task
+  private CreateAccountTask mAuthTask;
+
+  // UI references.
+  private Button mCreateAccountButton;
+  private Button mLoginPromptButton;
+  private EditText mEmailView;
+  private EditText mNameView;
+  private EditText mPasswordView;
+  private EditText mPasswordConfirmView;
+  private TextInputLayout mEmailViewLayout;
+  private TextInputLayout mNameViewLayout;
+  private TextInputLayout mPasswordViewLayout;
+  private TextInputLayout mPasswordConfirmViewLayout;
+  private ProgressDialog mProgressDialog;
+  private View mCreateAccountFormView;
 
   // Helper constants for argument variable names
   private static final String ARG_TITLE = "mTitle";
@@ -50,7 +82,181 @@ public class RegisterFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_register, container, false);
+    View createAccountView = inflater.inflate(R.layout.fragment_register, container, false);
+
+    // Email field
+    mEmailView = (EditText) createAccountView.findViewById(R.id.email_input);
+    mEmailViewLayout = (TextInputLayout) createAccountView.findViewById(R.id.email_layout);
+
+    // Name field
+    mNameView = (EditText) createAccountView.findViewById(R.id.name_input);
+    mNameViewLayout = (TextInputLayout) createAccountView.findViewById(R.id.name_layout);
+
+    // Password field
+    mPasswordView = (EditText) createAccountView.findViewById(R.id.password_input);
+    mPasswordViewLayout = (TextInputLayout) createAccountView.findViewById(R.id.password_layout);
+
+    // Password confirmation field
+    mPasswordConfirmView = (EditText) createAccountView.findViewById(R.id.password_confirm_input);
+    mPasswordConfirmViewLayout = (TextInputLayout) createAccountView.findViewById(R.id.password_confirm_layout);
+
+    // Create account button
+    mCreateAccountButton = (Button) createAccountView.findViewById(R.id.create_account_button);
+    mCreateAccountButton.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        doAccountCreate();
+      }
+    });
+
+    // Progress dialog
+    mProgressDialog = new ProgressDialog(getContext());
+    mProgressDialog.setMessage(getString(R.string.progress_message_create_account));
+
+    // Layout views
+    mCreateAccountFormView = (View) createAccountView.findViewById(R.id.create_account_form);
+
+    return createAccountView;
+  }
+
+  /**
+   * Validate the email address
+   * @param email Email address
+   * @return Whether email address is valid
+   */
+  private boolean isEmailValid(String email) {
+    //TODO: Replace with updated logic
+    return email.contains("@") && email.contains(".");
+  }
+
+  /**
+   * Validate the user's name
+   * @param name User name
+   * @return Whether name is valid
+   */
+  private boolean isNameValid(String name) {
+    // TODO: Replace with updated logic
+    return name.length() > 2;
+  }
+
+  /**
+   * Validate the password
+   * @param password Password
+   * @return Whether password is valid
+   */
+  private boolean isPasswordValid(String password) {
+    //TODO: Replace with updated logic
+    return password.length() > 4;
+  }
+
+  /**
+   * Validate the confirmation password
+   * @param password        Password
+   * @param passwordConfirm Confirmation password
+   * @return Whether the confirmation password matches
+   */
+  private boolean isPasswordConfirmValid(String password, String passwordConfirm) {
+    return password.equals(passwordConfirm);
+  }
+
+
+  /**
+   * Clear the inputs and errors
+   */
+  private void clearLoginInputs() {
+    mEmailView.setText("");
+    mEmailViewLayout.setError(null);
+
+    mNameView.setText("");
+    mNameViewLayout.setError(null);
+
+    mPasswordView.setText("");
+    mPasswordViewLayout.setError(null);
+
+    mPasswordConfirmView.setText("");
+    mPasswordConfirmViewLayout.setError(null);
+  }
+
+  /**
+   * Attempt to create an account
+   */
+  private void doAccountCreate() {
+    // Only permit one "Account Create" request at a time
+    if (mAuthTask != null) {
+      return;
+    }
+
+    // Reset errors.
+    mEmailViewLayout.setError(null);
+    mNameViewLayout.setError(null);
+    mPasswordViewLayout.setError(null);
+    mPasswordConfirmViewLayout.setError(null);
+
+    // Store values at the time of the login attempt.
+    String email = mEmailView.getText().toString();
+    String name = mNameView.getText().toString();
+    String password = mPasswordView.getText().toString();
+    String passwordConfirm = mPasswordConfirmView.getText().toString();
+
+    boolean cancel = false;
+    View focusView = null;
+
+    // Check for a valid password confirmation (must match), if the user entered one.
+    if (TextUtils.isEmpty(password)) {
+      mPasswordConfirmViewLayout.setError(getString(R.string.error_field_required));
+      focusView = mPasswordConfirmView;
+      cancel = true;
+    }
+    else if (!isPasswordConfirmValid(password, passwordConfirm)) {
+      mPasswordConfirmViewLayout.setError(getString(R.string.error_mismatching_passwords));
+      focusView = mPasswordConfirmView;
+      cancel = true;
+    }
+
+    // Check for a valid password, if the user entered one.
+    if (TextUtils.isEmpty(password)) {
+      mPasswordViewLayout.setError(getString(R.string.error_field_required));
+      focusView = mPasswordView;
+      cancel = true;
+    }
+    else if (!isPasswordValid(password)) {
+      mPasswordViewLayout.setError(getString(R.string.error_invalid_password));
+      focusView = mPasswordView;
+      cancel = true;
+    }
+
+    // Check for a valid email address
+    if (TextUtils.isEmpty(email)) {
+      mEmailViewLayout.setError(getString(R.string.error_field_required));
+      focusView = mEmailView;
+      cancel = true;
+    } else if (!isEmailValid(email)) {
+      mEmailViewLayout.setError(getString(R.string.error_invalid_email));
+      focusView = mEmailView;
+      cancel = true;
+    }
+
+    // Check for a valid name (is last to set focus properly).
+    if (TextUtils.isEmpty(name)) {
+      mNameViewLayout.setError(getString(R.string.error_field_required));
+      focusView = mNameView;
+      cancel = true;
+    } else if (!isNameValid(name)) {
+      mNameViewLayout.setError(getString(R.string.error_invalid_name));
+      focusView = mNameView;
+      cancel = true;
+    }
+
+    if (cancel) {
+      // Ignore create attempt (due to error) and set focus to last field with error
+      focusView.requestFocus();
+    } else {
+      // Show a progress spinner and start a background task to perform the account creation attempt
+      mProgressDialog.show();
+
+      mAuthTask = new CreateAccountTask(email, name, password);
+      mAuthTask.execute((Void) null);
+    }
   }
 
   // TODO: Rename method, update argument and hook method into UI event
@@ -91,5 +297,102 @@ public class RegisterFragment extends Fragment {
     // TODO: Update argument type and name
     void onFragmentInteraction(Uri uri);
   }*/
+
+
+  /**
+   * Represents an asynchronous account creation task used to create a user account
+   */
+  public class CreateAccountTask extends AsyncTask<Void, Void, Boolean> {
+
+    private final String mEmail;
+    private final String mName;
+    private final String mPassword;
+
+    CreateAccountTask(String email, String name, String password) {
+      mEmail = email;
+      mName = name;
+      mPassword = password;
+    }
+
+    @Override
+    protected Boolean doInBackground(Void... params) {
+      // TODO: attempt authentication against a network service.
+
+      try {
+        // Simulate network access.
+        Thread.sleep(2000);
+      } catch (InterruptedException e) {
+        return false;
+      }
+
+      try {
+        // Get the user file for modification (adding user)
+        Document usersFile = XMLFileUtils.getFile(getActivity().getBaseContext(), XMLFileUtils.USERS_FILE_NAME);
+        Element rootElement = usersFile.getRootElement();
+
+        List<Node> users = usersFile.selectNodes("/users/user");
+        boolean isUniqueUser = true;
+
+        // Verify that the user does not already exist
+        for (Node user : users) {
+          if (user.valueOf("@email").equals(mEmail)) {
+            isUniqueUser = false;
+            break;
+          }
+        }
+
+        // TODO: This needs to be modified to return a proper error (if possible)
+        if (!isUniqueUser) {
+          return false;
+        }
+
+        // Add the new user to the document
+        //  NOTE: This only works because it is the root element - how else could it work?
+        rootElement.addElement("user")
+            .addAttribute("email", mEmail)
+            .addAttribute("name", mName)
+            .addAttribute("password", mPassword);
+
+        // Write the modified file back out
+        XMLFileUtils.createFile(getActivity().getBaseContext(), XMLFileUtils.USERS_FILE_NAME, usersFile);
+
+        Log.d("MileageApp.auth", String.format("New user added: '%s', '%s', '%s'", mName, mEmail, mPassword));
+
+        return true;
+      } catch(Exception e) {
+        return false;
+      }
+    }
+
+    @Override
+    protected void onPostExecute(final Boolean success) {
+      mAuthTask = null;
+      mProgressDialog.dismiss();
+
+      if (success) {
+        // NOTE: Determine what to do on successful account creation (likely send email)
+        // TODO: Redirect to Login screen (now a tab)
+        //getActivity().finish();
+      } else {
+        mEmailViewLayout.setError(getString(R.string.error_account_already_exists));
+        mEmailViewLayout.requestFocus();
+      }
+
+      // TODO: Find why this doesn't work (even on failure)
+      // Define a snackbar based on the operation status
+      CharSequence snackbarResource = success
+          ? getString(R.string.success_create_account)
+          : getString(R.string.failure_create_account);
+      Snackbar resultSnackbar = Snackbar.make(mCreateAccountFormView, snackbarResource, Snackbar.LENGTH_SHORT);
+      resultSnackbar.show();
+    }
+
+    @Override
+    protected void onCancelled() {
+      // NOTE: Should it be possible to cancel Account Creation?
+      mAuthTask = null;
+      mProgressDialog.dismiss();
+    }
+  }
 }
 
